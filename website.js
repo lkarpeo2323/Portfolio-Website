0ea5c8493f96577a4efef39891ae5610173ad953
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-05-16 12:49:28 -0400

    Update website.js

--- website.js
+++ website.js
@@ -4,7 +4,7 @@ function LoadMenuInfo() {
 MyMenu += '<nav class="navbar navbar-inverse" id="navigation">';
 MyMenu += '  <div class="container">';
 MyMenu += '    <ul class="nav navbar-nav">';
-MyMenu += '      <li><a id="topText" class="glyphicon glyphicon-home" href="index"> Home</a></li>';
+MyMenu += '      <li><a id="topText" class="glyphicon glyphicon-home" href="index.html"> Home</a></li>';
 MyMenu += '      <li><a id="topText" class="glyphicon glyphicon-search" href="about.html"> About </a></li>';
 MyMenu += '      <li><a id="topText" class="glyphicon glyphicon-education" href="Academics"> Academics</a></li>';
 MyMenu += '      <li><a id="topText" class="glyphicon glyphicon-user" href="Contact"> Contact</a></li>';
