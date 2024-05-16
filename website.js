3ee4cf1248601942b19b48b24e9220636c4fd130
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-05-16 12:49:02 -0400

    Update website.js

--- website.js
+++ website.js
@@ -5,7 +5,7 @@ MyMenu += '<nav class="navbar navbar-inverse" id="navigation">';
 MyMenu += '  <div class="container">';
 MyMenu += '    <ul class="nav navbar-nav">';
 MyMenu += '      <li><a id="topText" class="glyphicon glyphicon-home" href="index"> Home</a></li>';
-MyMenu += '      <li><a id="topText" class="glyphicon glyphicon-search" href="about"> About </a></li>';
+MyMenu += '      <li><a id="topText" class="glyphicon glyphicon-search" href="about.html"> About </a></li>';
 MyMenu += '      <li><a id="topText" class="glyphicon glyphicon-education" href="Academics"> Academics</a></li>';
 MyMenu += '      <li><a id="topText" class="glyphicon glyphicon-user" href="Contact"> Contact</a></li>';
 MyMenu += '    </ul>';
