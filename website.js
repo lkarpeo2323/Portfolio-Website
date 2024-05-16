8b30e6918a0b732c72257d760bfda1f75e7552a4
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-05-16 12:48:11 -0400

    Update website.js

--- website.js
+++ website.js
@@ -4,10 +4,10 @@ function LoadMenuInfo() {
 MyMenu += '<nav class="navbar navbar-inverse" id="navigation">';
 MyMenu += '  <div class="container">';
 MyMenu += '    <ul class="nav navbar-nav">';
-MyMenu += '      <li><a id="topText" class="glyphicon glyphicon-home" href="Project Bootstrap.html"> Home</a></li>';
-MyMenu += '      <li><a id="topText" class="glyphicon glyphicon-search" href="About Me.html"> About </a></li>';
-MyMenu += '      <li><a id="topText" class="glyphicon glyphicon-education" href="Academics.html"> Academics</a></li>';
-MyMenu += '      <li><a id="topText" class="glyphicon glyphicon-user" href="Contact.html"> Contact</a></li>';
+MyMenu += '      <li><a id="topText" class="glyphicon glyphicon-home" href="index"> Home</a></li>';
+MyMenu += '      <li><a id="topText" class="glyphicon glyphicon-search" href="about"> About </a></li>';
+MyMenu += '      <li><a id="topText" class="glyphicon glyphicon-education" href="Academics"> Academics</a></li>';
+MyMenu += '      <li><a id="topText" class="glyphicon glyphicon-user" href="Contact"> Contact</a></li>';
 MyMenu += '    </ul>';
 MyMenu += '  </div>';
 MyMenu += '</nav>';
